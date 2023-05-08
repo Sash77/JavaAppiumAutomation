1d3dc5f36451308c58915a92182fcdd7eeea72bf
Sash77 <totsamiyya@yandex.ru>
2023-05-08 16:46:04 +0300

    Task_2

--- src/main/MainClassTest.java
+++ src/main/MainClassTest.java
@@ -1,13 +1,25 @@
 package main;
 
 import org.junit.Test;
+import test.MainClass;
 
 import static org.junit.Assert.assertEquals;
-import static test.MainClass.getLocalNumber;
+import static org.junit.Assert.assertTrue;
 
 public class MainClassTest {
+
+    int expectedValueLocal = 14;
+    int expectedValueClass = 45;
+    private MainClass mainClass = new MainClass();
+
     @Test
     public void testGetLocalNumber(){
-        assertEquals(14,getLocalNumber());
+        assertEquals(expectedValueLocal,mainClass.getLocalNumber());
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        assertTrue(String.format("Ожидаем, что %s больше, чем %s", mainClass.getClassNumber(), expectedValueClass),
+                mainClass.getClassNumber() >= expectedValueClass);
     }
 }
--- src/test/MainClass.java
+++ src/test/MainClass.java
@@ -1,7 +1,14 @@
 package test;
 
 public class MainClass {
-    public static int getLocalNumber(){
+
+    private int class_number = 20;
+
+    public int getLocalNumber(){
         return 14;
+    }
+
+    public int getClassNumber(){
+        return class_number;
     }
 }
